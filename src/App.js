d855da92beaaa2f664b57ab84d6218a9e6b9fdac
Fernando Alves <fernando.alves@jussi.com>
2019-04-09 22:34:40 -0300

    Challenge basic done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
-import SquareCollab from "./components/SquareCollab/SquareCollab";
+import BoardCollab from "./components/BoardCollab";
+import SquareCollab from "./components/SquareCollab";
 
-const App = () => <SquareCollab />;
+const App = () => (
+  <>
+    <BoardCollab />
+    <SquareCollab />
+  </>
+);
 
 export default App;
---
+++ src/components/BoardCollab/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BoardColab = () => (
+  <article>
+    <h2>Teste</h2>
+  </article>
+);
+
+export default BoardColab;
